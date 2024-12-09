5b3dcd82e29ad5d1a17706d6a71c4b0711bdd65f
zada <zada.wu@gmail.com>
2024-12-09 17:06:29 +0800

    add a router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,32 @@ import { RouterOutlet } from '@angular/router';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { CountdownComponent } from './countdown/countdown.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProgressBarComponent, CountdownComponent, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   template: `
-    <h1>{{ title }}</h1>
-    
-    <app-progress-bar [progress]="currentProgress" color="primary"></app-progress-bar>
-    <app-countdown [countdownNumber]="totalCountdown" (onDecrease)="updateProgress($event)" (onComplete)="countDownFinished()"></app-countdown>
-    <button (click)="incrementProgress()">增加进度</button>
-    <button (click)="resetProgress()">重置进度</button>
-    <app-home></app-home>
-    <router-outlet />
+    <main>
+      <header class="brand-name">
+        <h1>{{ title }}</h1>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+        <router-outlet />
+      </section>
+    </main>
   `,
   styleUrl: './app.component.scss'
 })
+
+/**
+ *     // <app-progress-bar [progress]="currentProgress" color="primary"></app-progress-bar>
+    // <app-countdown [countdownNumber]="totalCountdown" (onDecrease)="updateProgress($event)" (onComplete)="countDownFinished()"></app-countdown>
+    // <button (click)="incrementProgress()">增加进度</button>
+    // <button (click)="resetProgress()">重置进度</button>
+ */
 export class AppComponent {
   title = 'Angular-FireBase-Visualization';
   currentProgress = 0;
